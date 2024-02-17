223a3c4df87e68d812ff0bcdf95115d48061bf36
Agustin Cassani <agustinscassani@gmail.com>
2024-02-17 17:17:58 -0300

    TS enumerations used properly.

--- app/ui/title/Title.tsx
+++ app/ui/title/Title.tsx
@@ -6,5 +6,5 @@ export default function Title({ day, message }: { day: string, message?: string 
   // Utils
   const [dd, mm, dayNumber, yyyy] = new Date(`${day}T00:00:00`).toDateString().split(' ');
   // Render
-  return <p className={styles.title}>{message ? `${message} - ` : ''}{Days[dd]}, {Months[mm]} {dayNumber}, {yyyy}</p>;
+  return <p className={styles.title}>{message ? `${message} - ` : ''}{Days[dd as keyof typeof Days]}, {Months[mm as keyof typeof Months]} {dayNumber}, {yyyy}</p>;
 }
